3f91ae300c772041cf13740314596ed370923ba5
Minoru-Abe <tomominoru.abe@gmail.com>
2024-09-01 21:57:18 +0900

    commit up to step24

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -9,9 +9,27 @@ public class SayHelloController {
 	//"say-hello" => "Hello! What are you learning today?"
 	
 	//say-hello
-	@RequestMapping("say-hello")
+	@RequestMapping("say-hello-html")
 	@ResponseBody
 	public String sayHello() {
-		return "Hello! What are you learning today?";
+		StringBuffer sb = new StringBuffer();
+		sb.append("<html>");
+		sb.append("<head>");
+		sb.append("<title> My First HTML page</title>");
+		sb.append("</head>");
+		sb.append("<body>");
+		sb.append("My First html page with body");
+		sb.append("</body>");
+		sb.append("</html>");
+		
+		return sb.toString();
 	}
+	
+	//"say-hello-jsp" => sayHello.jsp
+	// /src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
+	@RequestMapping("say-hello-jsp")
+	public String sayHelloJsp() {
+		return "sayHello";
+	}
+	
 }
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title> My first HTML page</title>
+    </head>
+    <body>
+        My first html page with body in JSP
+    </body>
+</html>
