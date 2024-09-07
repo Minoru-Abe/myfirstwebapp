6463ff189ad5a7a02ed94e702709a62dcb2eec4e
Minoru-Abe <tomominoru.abe@gmail.com>
2024-09-07 15:43:48 +0900

    up to step36

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001, 'in28minutes', 'Get AWS Certified', CURRENT_DATE(), false );
+
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002, 'in28minutes', 'Get E-Certified', CURRENT_DATE(), false );
+
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003, 'in28minutes', 'Get JAVA Certified', CURRENT_DATE(), false );
+
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10004, 'in28minutes', 'Get container Certified', CURRENT_DATE(), false );
